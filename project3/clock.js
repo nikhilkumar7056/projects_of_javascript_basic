8f467885e5eca8f76566424fe2f9c964d740622a
Nikhil Kumar <141623909+nikhilkumar7056@users.noreply.github.com>
2026-01-18 13:16:50 +0000

    project3 some comments

--- project3/clock.js
+++ project3/clock.js
@@ -1,6 +1,10 @@
 const clock = document.getElementById('clock')
+// console.log(clock)
 
-setInterval(() => {
-    const date = new Date()
-    clock.innerHTML = date.toLocaleTimeString()
-}, 1000);
+// const date = new Date();
+// console.log(date.toLocaleTimeString())
+setInterval(function(){ //use to set the time interval in which function work
+  const date = new Date()
+  // console.log(date.toLocaleTimeString())
+  clock.innerHTML = date.toLocaleTimeString()
+},1000)
